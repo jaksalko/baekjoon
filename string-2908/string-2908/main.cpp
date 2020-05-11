07be69433236f658569ad0149b8f5dad44150e4c
정문경 <jaksal95@naver.com>
2020-05-11 18:50:18 +0900

    2908

--- string-2908/string-2908/main.cpp
+++ string-2908/string-2908/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    char temp;
+    vector<int> v;
+    string s;
+    cin >> s;
+    temp = s[0];
+    s[0] = s[2];
+    s[2] = temp;
+    
+    v.push_back(stoi(s));
+    cin >> s;
+    temp = s[0];
+    s[0] = s[2];
+    s[2] = temp;
+    v.push_back(stoi(s));
+    
+    
+    int mx = *max_element(v.begin(),v.end());;
+    cout << mx;
+    
+    
     return 0;
 }
