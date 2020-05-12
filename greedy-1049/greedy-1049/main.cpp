91107b4a3affc1d68c65b83f42476cde75d89ef1
정문경 <jaksal95@naver.com>
2020-05-12 18:50:24 +0900

    gitignore

---
+++ greedy-1049/greedy-1049/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  greedy-1049
+//
+//  Created by 정문경 on 2020/05/12.
+//  Copyright © 2020 정문경. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
