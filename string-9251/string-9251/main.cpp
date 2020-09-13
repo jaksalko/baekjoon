6716e7c7005c6bf3fd25af6e6f9b92e27fa92541
jaksalko <jaksal95@naver.com>
2020-09-13 18:47:57 +0900

    LCS

--- string-9251/string-9251/main.cpp
+++ string-9251/string-9251/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string ff;
+    string ss;
+    
+    int dp[1001][1001];
+    int answer = 0;
+    
+    cin >> ff >> ss;
+    
+    for(int i = 1 ; i <= ff.size() ; i++)
+    {
+        for(int j = 1 ; j <= ss.size() ; j++)
+        {
+            if(ff[i-1] == ss[j-1])
+            {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else
+            {
+                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+            }
+             answer = max(answer,dp[i][j]);
+        }
+       
+    }
+    
+    cout << answer;
+    
     return 0;
 }
